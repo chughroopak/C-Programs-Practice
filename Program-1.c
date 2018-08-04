d97ee059fefe6988aa847b74d3f73aa37854678e
Roopak Chugh <chughroopak@gmail.com>
2018-08-04 17:41:19 +0530

    Update Program 1

---
+++ Program-1.c
@@ -0,0 +1,47 @@
+// Program 1: Write a program to sort a number of strings using bubble sort. 
+// Input is a number of strings and the output is the sorted list based on the length of strings.  
+// For e.g.: If input is jyoti, sareeka, anisha, sangita, savita, suja  
+// The output is suja, jyoti, anisha, savita, sareeka, sangita
+
+#include<stdio.h>
+#include<string.h>
+
+#define MAX 100
+ 
+void sortStrings(char arr[][MAX], int n)
+{
+    char temp[MAX];
+ 
+    // Sorting strings using bubble sort
+    for (int j=0; j<n-1; j++)
+    {
+        for (int i=j+1; i<n; i++)
+        {
+            // if (strcmp(arr[j], arr[i]) > 0)
+            // {
+            //     strcpy(temp, arr[j]);
+            //     strcpy(arr[j], arr[i]);
+            //     strcpy(arr[i], temp);
+            // }
+            if (strlen(arr[j])>strlen(arr[i]))
+            {
+                strcpy(temp, arr[j]);
+                strcpy(arr[j], arr[i]);
+                strcpy(arr[i], temp);
+            }        
+            
+        }
+    }
+}
+
+int main()
+{
+    char arr[][MAX] = {"jyoti", "sareeka", "anisha", "sangita", "savita", "suja"};
+    int n = sizeof(arr)/sizeof(arr[0]);
+ 
+    sortStrings(arr, n);
+ 
+    for (int i=0; i<n; i++)
+        printf("%s, ", arr[i]);
+    return 0;
+}
